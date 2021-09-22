257e9af4c68e158ff1342af4f06173b6daaec0fa
Jessica2603 <91164392+Jessica2603@users.noreply.github.com>
2021-09-22 11:11:00 -0300

    Update HelloWorld.java

--- src/basico/HelloWorld.java
+++ src/basico/HelloWorld.java
@@ -3,7 +3,8 @@ package basico;
 public class HelloWorld {
 	public static void main(String[]args) {
 		System.out.println("Hello World!");
-		
+		System.out.println("I`m adding code to my project");
+		System.out.println("Adding code from GitHub");
 	
 	}
 
